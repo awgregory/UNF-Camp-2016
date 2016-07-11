3ba8c0910890d4b4ca36c990917514a1105b4a9d
Adrian Gregory <adrianwgregory2@gmail.com>
2016-07-11 17:36:31 -0400

    Adding two SQL Query files

---
+++ Database and Entity Framework/BlogDemoQuery1.sql
@@ -0,0 +1,21 @@
+﻿--Saved Queries require you re-connect in VS to run || Choose Server (localdb)\MSSQLLocalDB || and BlogDemo 
+-- See History for saved connection
+
+--INSERT INTO BlogPosts (Title, Body)
+--VALUES
+--	('First Post', 'This is my first post!'),
+--	('Second Post', 'This is my second post!')
+
+SELECT * from BlogPostComments
+
+SELECT * from BlogPosts
+
+
+--INSERT INTO BlogPostComments (BlogPostID, Comment)
+--VALUES (1, 'This post is great')
+
+--SELECT bp.Id, bp.TITLE, bpc.Comment
+--FROM BlogPosts bp
+--INNER JOIN BlogPostComments bpc ON bp.Id = bpc.BlogPostId
+
+--DELETE FROM BlogPostComments WHERE BlogPostId = 1
---
+++ Database and Entity Framework/DatabaseEntityFrameworkDemoApp/SQLQuery2.sql
@@ -0,0 +1,29 @@
+﻿--Saved Queries require you connect again when you run || Choose Server (localdb)\MSSQLLocalDB and BlogDemo || See History for saved connection
+
+-- You don't have to comment all sql queries out, just hightlight the one you want to run
+
+
+--INSERT INTO BlogPosts (Title, Body)
+--VALUES
+--	('First Post', 'This is my first post!'),
+--	('Second Post', 'This is my second post!')
+
+--SELECT * from BlogPostComments
+
+--select * from blogposts
+--where BlogPosts.BODY
+--like '%post%'
+
+
+--INSERT INTO BlogPostComments (BlogPostID, Comment)
+--VALUES (1, 'This post is great')
+
+SELECT bp.Id, bp.TITLE, bpc.Comment
+FROM BlogPosts bp
+INNER JOIN BlogPostComments bpc ON bp.Id = bpc.BlogPostId
+
+SELECT bp.Id, bp.TITLE, bpc.Comment
+FROM BlogPosts bp
+LEFT JOIN BlogPostComments bpc ON bp.Id = bpc.BlogPostId
+
+--DELETE FROM BlogPostComments WHERE BlogPostId = 1
